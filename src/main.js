d127b88ee4bc9fb22f47b07a38326c42dc3c2d6a
GitDaisy <100Daisy@protonmail.com>
2023-07-13 19:35:19 +0200

    icons: switch to fontawesome icon pack

--- src/main.js
+++ src/main.js
@@ -4,7 +4,17 @@ import App from './App.vue'
 import Oruga from '@oruga-ui/oruga-next'
 import '@oruga-ui/oruga-next/dist/oruga-full-vars.css'
 import '@mdi/font/css/materialdesignicons.min.css'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas, fab);
 
 createApp(App)
-    .use(Oruga)
-    .mount('#app')
+  .component("VueFontawesome", FontAwesomeIcon)
+  .use(Oruga, {
+    iconPack: 'fas',
+    iconComponent: 'vue-fontawesome'
+  })
+  .mount("#app");
