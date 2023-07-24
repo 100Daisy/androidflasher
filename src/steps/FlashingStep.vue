c798cace8b0d85d5962776cc48d977305eca2f1c
GitDaisy <100Daisy@protonmail.com>
2023-07-24 18:22:15 +0200

    steps: flashing: Update to both configurators

--- src/steps/FlashingStep.vue
+++ src/steps/FlashingStep.vue
@@ -8,31 +8,34 @@ import ProgressBar from '@/components/ProgressBar.vue';
 import FlashLog from '@/components/FlashLog.vue';
 
 const deviceStore = useDeviceStore();
+const device = deviceStore.device;
 const latestLine = ref('');
 const progress = ref(0);
-const data = deviceStore.flashObject;
+const data = deviceStore.flashObject.files;
 
 async function startFlashing() {
   progress.value = 0;
   let curr_progress = -1;
-  for (let i = 0; i < data.quantity; i++) {
+  for (let i = 0; i < data.length; i++) {
     curr_progress++;
-    await deviceStore.device.runCommand(`set_active:${data.files[i].slot}`);
-    latestLine.value = `Flashing ${data.files[i].filename}...`;
-    if (data.options.disableVerity && data.files[i].partition == 'vbmeta') {
-      const vbmeta = await disableVerifyVbmeta(data.files[i].blob);
-      await deviceStore.device.flashBlob(data.files[i].partition, vbmeta, (t) => {
+    console.log(device)
+    await device.runCommand(`set_active:${data[i].slot}`);
+    latestLine.value = `Flashing ${data[i].filename}...`;
+    if (data.options?.disableVerity && data[i].partition == 'vbmeta') {
+      const vbmeta = await disableVerifyVbmeta(data[i].blob);
+      await device.flashBlob(data[i].partition, vbmeta, (t) => {
         // take progress.value every iteration and add progress to it
         progress.value = curr_progress + t;
       })
       continue;
     }
-    await deviceStore.device.flashBlob(data.files[i].partition, data.files[i].blob, (t) => {
+    await device.flashBlob(data[i].partition, data[i].blob, (t) => {
+      console.log(t)
       // take progress.value every iteration and add progress to it
       progress.value = curr_progress + t;
     })
   }
-  if (data.options.cleanFlash) {
+  if (data.options?.cleanFlash) {
     latestLine.value = 'Erasing userdata...';
     await deviceStore.device.runCommand(`erase:userdata`);
   }
@@ -57,7 +60,7 @@ startFlashing().then(() => {
     <main>
       <o-icon icon="bolt" size="large" />
       <FlashLog :latest-line="latestLine" :opacity-decrease="0.3" :maxLines="6"/>
-      <ProgressBar :progress="progress" :parts="data.quantity"/>
+      <ProgressBar :progress="progress" :parts="data.length"/>
     </main>
 </template>
 
